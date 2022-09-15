604caf1005d779465774703cd9bd2245b5a54654
suneruperera0 <107423004+suneruperera0@users.noreply.github.com>
2022-09-15 08:41:21 -0400

    Add files via upload

---
+++ media.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Setting Image Size
+    |--------------------------------------------------------------------------
+    | [width X height]
+    |
+    | Thumbnail (default 250px x 250px)
+    | Feature Image (default 500px x 350px)
+    | Original image resolution (unmodified)
+    |
+    */
+
+    'size' => [
+        'thumbnail'             => [250, 250],
+        'feature_image'         => [500, 350], //Main Product
+        //'full' => null,
+    ],
+
+
+];
